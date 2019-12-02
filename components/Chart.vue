b09f1b04f861797a6a02853b4c80c44b4e84838c
djeromhal <mikhailivanko@gmail.com>
2019-12-02 13:14:03 +0300

    qavala test

--- components/Chart.vue
+++ components/Chart.vue
@@ -15,16 +15,35 @@ export default {
   name: 'ChartFB',
   mounted() {
     let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
+    let interfaceColors = new am4core.InterfaceColorSet();
 
     chart.paddingRight = 20;
+
+    chart.leftAxesContainer.layout = "vertical";
 
     chart.dateFormatter.inputDateFormat = "k:m";
 
     let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
     dateAxis.renderer.grid.template.location = 0;
+    dateAxis.renderer.ticks.template.length = 8;
+    dateAxis.renderer.ticks.template.strokeOpacity = 0.1;
+    // dateAxis.renderer.grid.template.disabled = true;
+    dateAxis.renderer.ticks.template.disabled = false;
+    dateAxis.renderer.ticks.template.strokeOpacity = 0.2;
+    dateAxis.renderer.fontSize = "0.8em"
+    // dateAxis.baseInterval = {
+    //   "timeUnit": "minute",
+    //   "count": 1
+    // };
+    dateAxis.renderer.grid.template.location = 0;
 
     let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
     valueAxis.tooltip.disabled = true;
+    valueAxis.height = am4core.percent(60);
+    valueAxis.renderer.gridContainer.background.fill = interfaceColors.getFor("alternativeBackground");
+    valueAxis.renderer.gridContainer.background.fillOpacity = 0.02;
+    valueAxis.title.text = 'Close';
+    valueAxis.renderer.fontSize = "0.8em"
 
     let series = chart.series.push(new am4charts.CandlestickSeries());
     series.dataFields.dateX = "minute";
@@ -43,6 +62,21 @@ export default {
     lineSeries.dataFields.valueY = "close";
     // need to set on default state, as initially series is "show"
     lineSeries.defaultState.properties.visible = false;
+
+    // Create volume
+    let valueAxis2 = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis2.tooltip.disabled = true;
+    valueAxis2.height = am4core.percent(40);
+    valueAxis2.marginTop = 60;
+    valueAxis2.renderer.gridContainer.background.fill = interfaceColors.getFor("alternativeBackground");
+    valueAxis2.renderer.gridContainer.background.fillOpacity = 0.05;
+    valueAxis2.title.text = 'Volume';
+    valueAxis2.renderer.fontSize = "0.8em"
+
+    var volumeSeries = chart.series.push(new am4charts.ColumnSeries());
+    volumeSeries.dataFields.valueY = "volume";
+    volumeSeries.dataFields.dateX = "minute";
+    volumeSeries.yAxis = valueAxis2;
 
     // hide from legend too (in case there is one)
     lineSeries.hiddenInLegend = true;
